b1bef5a331d3a9cc1586fcac916d470078b64c07
manij <manij@MIS-MANI>
2020-07-11 16:17:29 +0800

    Updates

---
+++ precache-manifest.c6e8b17c12e37025a09f29971e01cf29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a4e4d0337adf5962a83244ac639d8b3",
+    "url": "/smart-dropdown/index.html"
+  },
+  {
+    "revision": "ec63f39ebd67804fec11",
+    "url": "/smart-dropdown/static/css/main.8ac48c96.chunk.css"
+  },
+  {
+    "revision": "711c6144e3afbce7d707",
+    "url": "/smart-dropdown/static/js/2.2e4b6020.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/smart-dropdown/static/js/2.2e4b6020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec63f39ebd67804fec11",
+    "url": "/smart-dropdown/static/js/main.3f7b98d5.chunk.js"
+  },
+  {
+    "revision": "326316561b4d751c0a5f",
+    "url": "/smart-dropdown/static/js/runtime-main.d55986a5.js"
+  }
+]);
