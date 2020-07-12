6607b6312c12b9db878e3dccc1a4dd7d60a13917
manij <manij@MIS-MANI>
2020-07-12 12:50:57 +0800

    api endpoint added

--- src/api/countries.js
+++ src/api/countries.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getCountries = () => {
   return new Promise((resolve, reject) => {
     axios
-      .get("https://run.mocky.io/v3/fb01583b-16dc-4d2a-abc8-fc2559015e1a")
+      .get("https://countries.free.beeceptor.com/countries")
       .then(resolve)
       .catch(reject);
   });
