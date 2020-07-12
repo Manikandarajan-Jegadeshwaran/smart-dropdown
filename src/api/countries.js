4c703b86077b64281e1787495037d639db9c2559
manij <manij@MIS-MANI>
2020-07-12 13:15:03 +0800

    api endpoint changes

--- src/api/countries.js
+++ src/api/countries.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getCountries = () => {
   return new Promise((resolve, reject) => {
     axios
-      .get("https://countries.free.beeceptor.com/countries")
+      .get("https://demo4556897.mockable.io/countries")
       .then(resolve)
       .catch(reject);
   });
