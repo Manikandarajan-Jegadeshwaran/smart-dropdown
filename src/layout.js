0c9a1b650719d19549deb0972289cae198661808
manij <manij@MIS-MANI>
2020-07-12 13:47:41 +0800

    repo link added to app

--- src/layout.js
+++ src/layout.js
@@ -1,10 +1,36 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    "& a": {
+      padding: 10,
+      color: "#0000004f",
+      cursor: "pointer",
+    },
+    "& a:hover": {
+      color: "#000",
+    },
+  },
+}));
 
 function Layout(props) {
+  const classes = useStyles();
   return (
     <Container maxWidth='lg'>
-      <h2>Smart Drop Down</h2>
+      <div className={classes.header}>
+        <h2>Smart Drop Down</h2>
+        <a
+          title='Open smart-dropdown github repo'
+          href='https://github.com/Manikandarajan-Jegadeshwaran/smart-dropdown'
+        >
+          <GitHubIcon />
+        </a>
+      </div>
       {props.children}
       <div
         style={{
